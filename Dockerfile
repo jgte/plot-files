7b209f4bda6ef5acda6993a3974262b538315235
jgte <5040364+jgte@users.noreply.github.com>
2020-05-01 12:20:34 +0200

    using dockerize script with all needed docker operations

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM alpine:3.9.6
-
-RUN apk update && apk upgrade && apk add gnuplot git bash
-
-RUN cd $HOME && git clone https://github.com/jgte/plot-files.git
-
-ENV PATH "$HOME/plot-files:$PATH"
---
+++ dockerize.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+case "$1" in
+  "modes" ) #anchor
+    grep '#anchor' $BASH_SOURCE  
+  ;;
+  "test" ) #anchor
+    ./test/test-plot-files.sh && mv ./test/test.png .
+  ;;
+  "dockerfile") #anchor
+  echo "\
+FROM alpine:3.9.6
+
+RUN apk update \
+  && apk upgrade \
+  && apk add --no-cache gnuplot git bash
+
+WORKDIR /plot-files
+
+RUN git clone https://github.com/jgte/plot-files.git . && rm -fr .git/
+
+ENTRYPOINT [\"./$(basename $BASH_SOURCE)\"]
+
+CMD [\"help\"]
+"
+  ;;
+  "build") #anchor
+    VERSION=$(git log --pretty=format:"%as" | head -n1)
+    $BASH_SOURCE dockerfile > Dockerfile
+    docker build -t  spacegravimetry/plot-files:$VERSION .
+  ;;
+  *)
+    ./plot-files.sh "$@"
+  ;;
+esac
