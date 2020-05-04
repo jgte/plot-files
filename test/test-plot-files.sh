1dd13513c6c0915fdc4614a4def1d14a0790f569
jgte <5040364+jgte@users.noreply.github.com>
2020-05-04 15:56:32 +0200

    writing output plot in $PWD

--- test/test-plot-files.sh
+++ test/test-plot-files.sh
@@ -5,5 +5,5 @@ DAT=$DIR/test.dat
 
 $(dirname $DIR)/plot-files.sh $DAT -labels="-,-,t,-,x,y,z,-" \
   -title="Calibrated acc GRACE-A 2008-08-01 arc-01" \
-  -out=${DAT%.dat} demean force -point-size=0 \
+  -out=$PWD/$(basename ${DAT%.dat}) demean force -point-size=0 \
   -xticks=integer -xlabel="seconds of day" -ylabel="[m/s^2]" $@
