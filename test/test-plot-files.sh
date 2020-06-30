cd6c3b1354561df7e2233d94f7881232a050171b
jgte <5040364+jgte@users.noreply.github.com>
2020-06-30 19:04:07 +0200

    updated the test script to handle the new argument format

--- test/test-plot-files.sh
+++ test/test-plot-files.sh
@@ -3,7 +3,15 @@
 DIR=$(cd $(dirname $BASH_SOURCE);pwd)
 DAT=$DIR/test.dat
 
-$(dirname $DIR)/plot-files.sh $DAT -labels="-,-,t,-,x,y,z,-" \
-  -title="Calibrated acc GRACE-A 2008-08-01 arc-01" \
-  -out=$DIR/$(basename ${DAT%.dat}) demean force -point-size=0 \
-  -xticks=integer -xlabel="seconds of day" -ylabel="[m/s^2]" $@
+$(dirname $DIR)/plot-files.sh \
+  --files $DAT \
+  --labels "-,-,t,-,x,y,z,-" \
+  --title "Calibrated acc GRACE-A 2008-08-01 arc-01" \
+  --out $DIR/$(basename ${DAT%.dat}) \
+  --demean \
+  --force \
+  --point-size 0 \
+  --xticks integer \
+  --xlabel "seconds of day" \
+  --ylabel="[m/s^2]" \
+  $@
