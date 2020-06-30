5445315564bcf01ed4ca6f8c323516dc0ec2744a
jgte <5040364+jgte@users.noreply.github.com>
2020-06-30 19:02:50 +0200

    adapted the entry point to handle different scripts in the container

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,22 +5,25 @@ APPDIR=$($DIR/dockerize.sh app-dir)
 IODIR=$($DIR/dockerize.sh io-dir)
 
 case "$1" in
+  sh) #run the shell instead of plot-files.sh
+    exec /bin/bash -i
+  ;;
   modes) #shows all available modes
     grep ') #' $BASH_SOURCE \
       | grep -v grep \
       | sed 's:)::g' \
       | column -t -s\#
   ;;
-  test) #test plot-files.sh
-    exec $APPDIR/test/test-plot-files.sh -outdir=$IODIR
+  apps) #slows all avalable apps
+    exec for i in $(ls $APPDIR/plot-*); do basename $i; done
   ;;
-  cat-test|example) #shows the test script
-    exec cat $APPDIR/test/test-plot-files.sh 
+  test-*) #tests an app (some may not yet have a test)
+    exec $APPDIR/test/$1 -outdir=$IODIR
   ;;
-  sh) #run the shell instead of plot-files.sh
-    exec /bin/bash -i
+  example-*) #shows the test script of an app
+    exec cat $APPDIR/test/test-${i/example-}
   ;;
   *) #transparently pass all other arguments to ./plot-files.sh
-    exec $APPDIR/plot-files.sh "$@" -outdir=$IODIR
+    exec $APPDIR/$i "$@" -outdir=$IODIR
   ;;
 esac
