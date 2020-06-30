aa0f76038d9c73875301e283f8e1d983ee9a0d75
jgte <5040364+jgte@users.noreply.github.com>
2020-06-30 20:12:42 +0200

    routine development at Tue Jun 30 20:12:42 CEST 2020

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,7 @@ APPDIR=$($DIR/dockerize.sh app-dir)
 IODIR=$($DIR/dockerize.sh io-dir)
 
 case "$1" in
-
-  sh) #run the shell instead of plot-files.sh
+  sh) #run the shell instead of an app
     exec /bin/bash -i
   ;;
   modes) #shows all available modes
@@ -18,11 +17,24 @@ case "$1" in
   apps) #slows all avalable apps
     for i in $(ls $APPDIR/plot-*); do basename $i; done
   ;;
-  test-*) #tests an app (some may not yet have a test)
+  test-*) #tests an app; some may not yet have a test
     exec $APPDIR/test/$1 -outdir=$IODIR
   ;;
   example-*) #shows the test script of an app
-    exec cat $APPDIR/test/test-${i/example-}
+    exec cat $APPDIR/test/test-${1/example-}
+  ;;
+  help)
+    echo "\
+Possible arguments:
+- mode
+- app-name app-args
+
+mode is one of:
+$(BASH_SOURCE modes)
+
+app-name is one of:
+$(BASH_SOURCE apps)
+"
   ;;
   *) #transparently pass all other arguments to ./plot-files.sh
     exec $APPDIR/$i "$@" -outdir=$IODIR
