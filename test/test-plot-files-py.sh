e2786734aefe0816fe83cf98f50eb0515e63fd53
jgte <5040364+jgte@users.noreply.github.com>
2020-08-27 10:59:57 +0200

    updated test to produce files with different names and consistent with any optional input arguments (such as --demean)

--- test/test-plot-files-py.sh
+++ test/test-plot-files-py.sh
@@ -1,8 +1,35 @@
 #!/bin/bash -ue
 
 DIR=$(cd $(dirname $BASH_SOURCE);pwd)
-
 DAT=$DIR/test.dat
+
+# std test
+# NOTICE: the std values below were derived with:
+# file-stats.awk test.dat | grep std: | awk '{print $6,$7,$8}'
+STDx=5.29369e-09
+STDy=8.47663e-09
+STDz=9.18032e-09
+DAT2=$DIR/test2.dat
+awk '{print $3,$5,'$STDx',$6,'$STDy',$7,'$STDz'}' $DAT > $DAT2
+ARGS=(
+  --files $DAT2
+  --labels t,x,std,y,std,z,std
+  --title "Calibrated acc GRACE-A 2008-08-01 arc-01"
+  --x-label "seconds of day"
+  --y-label "[m/s^2]"
+  --grid
+  $@
+)
+#NOTICE: need to retrieve the automatic file name because $@ can include --demean,
+#        which would be ignored since --out is set explicitly to make sure this plot
+#        is not over-written by further tests
+OUT=$($(dirname $DIR)/plot-files.py "${ARGS[@]}" --out-name)
+$(dirname $DIR)/plot-files.py "${ARGS[@]}" \
+  --debug \
+  --timing \
+  --out ${OUT%.png}.std.png
+
+# --psa test
 $(dirname $DIR)/plot-files.py \
   --files $DAT \
   --labels "\-,-,t,-,x,y,z,-" \
@@ -16,6 +43,7 @@ $(dirname $DIR)/plot-files.py \
   --logy \
   $@
 
+# --diff test
 $(dirname $DIR)/plot-files.py \
   --files $DAT \
   --labels "\-,-,t,-,x,y,z,-" \
@@ -28,25 +56,8 @@ $(dirname $DIR)/plot-files.py \
   --diff \
   $@
 
-#NOTICE: the std values below were derived with:
-# file-stats.awk test.dat | grep std: | awk '{print $6,$7,$8}'
-STDx=5.29369e-09
-STDy=8.47663e-09
-STDz=9.18032e-09
-DAT2=$DIR/test2.dat
-awk '{print $3,$5,'$STDx',$6,'$STDy',$7,'$STDz'}' $DAT > $DAT2
-$(dirname $DIR)/plot-files.py \
-  --files $DAT2 \
-  --labels "t,x,std,y,std,z,std" \
-  --title "Calibrated acc GRACE-A 2008-08-01 arc-01" \
-  --x-label "seconds of day" \
-  --y-label "[m/s^2]" \
-  --grid \
-  --debug \
-  --timing \
-  $@
 
-
+# --html test
 $(dirname $DIR)/plot-files.py \
   --files $DAT \
   --labels "\-,-,t,-,x,y,z,-" \
@@ -57,7 +68,6 @@ $(dirname $DIR)/plot-files.py \
   --debug \
   --timing \
   --html \
-  --force \
   $@
 
   # --demean \
